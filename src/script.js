e267cf7e6832b9103e4b443eb251af5484e11016
JamesVictor-o <victorjames40v@gmail.com>
2024-11-08 05:46:12 +0100

    add buyers to record book

--- src/script.js
+++ src/script.js
@@ -11,43 +11,77 @@ let isReadOnly=false
 
 // function for selling doughnuts
 function makeSells(){
+    let pricePerdoughnut=document.getElementById("changePrice").value
     let buyersName=document.getElementById("buyersName");
     let quantityOfDoughnut=document.getElementById("doughNutQuantity");
-
-    let buyersInfo={
-        buyersName:buyersName.value,
-        quantityOfDoughnut:quantityOfDoughnut.value
+    
+    // validating sells details before selling
+    if(pricePerdoughnut.trim()=="") return alert("please set a price")
+    if(buyersName.value.trim()==""|| quantityOfDoughnut.value.trim()=="") return alert("please fill the required sells details")    
+    
+    // checking for already existing buyers
+    let buyers_Index=recordKeeper.findIndex(buyer => buyer.buyersName === buyersName.value)   
+    if(buyers_Index < 0){
+        let buyersInfo={
+            buyersName:buyersName.value,
+            quantityOfDoughnut:Number(quantityOfDoughnut.value),
+            amount:Number(quantityOfDoughnut.value) * Number(pricePerdoughnut),
+            pricePerdoughnut:Number(pricePerdoughnut)
+        }
+        recordKeeper.push(buyersInfo)
+    }else{
+        for(i=0;i< recordKeeper.length; i++){
+            if(recordKeeper[i].buyersName.trim() === buyersName.value.trim()){
+                recordKeeper[i].quantityOfDoughnut = recordKeeper[i].quantityOfDoughnut + Number(quantityOfDoughnut.value);
+                recordKeeper[i].amount=recordKeeper[i].quantityOfDoughnut * pricePerdoughnut
+            }
+        }
     }
-    recordKeeper.push(buyersInfo)
+   
     // clear the input field
      buyersName.value="";
      quantityOfDoughnut.value=""
 
-    
     TotalValue(recordKeeper) 
 };
 
 // function for summing up total number of doughnuts sold
 function TotalValue(array){
-    let availableQuantity=document.querySelector("#availableQuantity")
-    let totalDoughtnutSold=array.reduce((acc,cur)=>{
-        return acc + Number(cur.quantityOfDoughnut)
-    },0)
-     let avl=totalDoughts                                                                                                                                                             - totalDoughtnutSold
-     if(avl == 0) return alert("you self don try, go look for better thing chop")
+    let availableQuantity=document.querySelector("#availableQuantity");
+    let inputedQuantity=document.querySelector("#changeQuantity").value
 
-    availableQuantity.innerHTML =avl
+    let totalDoughtnutSold=array.reduce((acc,cur)=>{
+        return acc + cur.quantityOfDoughnut
+    },0)
+     
+     let avl=inputedQuantity-totalDoughtnutSold;
+     if(avl == 0) return alert("you self don try, go look for better thing chop")
+    availableQuantity.innerHTML =avl;
+
+   console.log(array)
 }
 
 // function to updatePrice and quantity
 function updatePriceQuantity(btn){
+    // updating the number of available doughnuts
+   
+    // --------------------------------------------------//
+   
+
     let classF=btn.name
+
+    if(classF == 'changeQuantity'){
+        let availableQuantity=document.querySelector("#availableQuantity");
+        let inputedQuantity=document.querySelector("#changeQuantity").value;
+        availableQuantity.innerHTML=inputedQuantity;
+    }
    let inputPrice=document.getElementById(classF);
    isReadOnly=!isReadOnly
 
    inputPrice.readOnly=isReadOnly
 
    btn.innerText= isReadOnly ? "Edit": "Edited" ;
+
 }
 
 editBtns.forEach(btn=>{
